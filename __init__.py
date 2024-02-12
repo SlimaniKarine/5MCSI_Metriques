c21a1a9de706eab94018e2d052536462b8620a75
SlimaniKarine <93348357+SlimaniKarine@users.noreply.github.com>
2024-02-12 17:32:15 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ def hello_world():
   
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"  
+   return render_template('contact.html') #COMMENTAIRE2
   
 if __name__ == "__main__":
   app.run(debug=True)
